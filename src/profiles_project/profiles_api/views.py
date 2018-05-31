1be46f37d16f49b7f51eee667aba5f8d290e9881
Anthony Alberti <aalberti333@gmail.com>
2018-05-31 17:07:01 -0400

    Added create, retrieve, update, partial update, and destroy methods to our viewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -60,6 +60,8 @@ class HelloApiView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """Test api viewset"""
 
+    serializer_class = serializers.HelloSerializer
+
     #these don't use http functions
     #they use different functions you would perform on an object
     #
@@ -69,9 +71,41 @@ class HelloViewSet(viewsets.ViewSet):
         """return a hello message"""
 
         a_viewset = [
-            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Uses actions (list, create, retrieve, update, partial_update, delete)',
             'Automatically maps to URLs using Routers',
             'provides more functionality with less code'
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+
+    def create(self, request):
+        """create a new hello message."""
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """handles getting an object by its ID."""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """handles updating an object"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """handles updating part of the object."""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handles removing an object"""
+
+        return Response({'http_method': 'DELETE'})
